dbbe28ec3b678882d03b07b7a315ecf610981ea4
Eddie Jung <ejung716@gmail.com>
2021-08-23 16:02:17 -0700

    initial commit. Add nav controller and mkMapView to viewController.

---
+++ Project16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by Eddie Jung on 8/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
