51990a26e53535aa3c64260b8d1d9b3f0abd1c01
Eddie Jung <ejung716@gmail.com>
2021-08-23 16:54:27 -0700

    Update pin color to blue by typecasting annotationView to MKPinAnnotationView.

--- Project16/ViewController.swift
+++ Project16/ViewController.swift
@@ -28,11 +28,12 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         let identifier = "Capital"
         
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
         
         if annotationView == nil {
             annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
             annotationView?.canShowCallout = true
+            annotationView?.pinTintColor = .blue
             
             let btn = UIButton(type: .detailDisclosure)
             annotationView?.rightCalloutAccessoryView = btn
